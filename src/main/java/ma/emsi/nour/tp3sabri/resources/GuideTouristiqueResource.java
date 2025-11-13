4252731e7149a8b5b05e5741989a6ec8180be367
NourSabriEmsi <Nour.Sabri@emsi-edu.ma>
2025-11-13 16:31:09 +0100

    Create the villeoupays class and rename the HelloApplication class using RestConfig

---
+++ src/main/java/ma/emsi/nour/tp3sabri/resources/GuideTouristiqueResource.java
@@ -0,0 +1,22 @@
+package ma.emsi.nour.tp3sabri.resources;
+
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import ma.emsi.nour.tp3sabri.llm.LlmClientPourGemini;
+
+@Path("/guide")
+public class GuideTouristiqueResource {
+
+    @Inject
+    LlmClientPourGemini llm;
+
+    @GET
+    @Path("lieu/{ville_ou_pays}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response villeOuPays(@PathParam("ville_ou_pays") String lieu) {
+        return Response.ok(lieu).build();
+    }
+}
