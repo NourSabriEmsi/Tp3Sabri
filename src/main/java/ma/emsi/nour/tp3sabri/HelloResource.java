31ae87b9276163eead846aef92f82f92667a7e50
NourSabriEmsi <Nour.Sabri@emsi-edu.ma>
2025-11-13 15:27:26 +0100

    Comment out the return statement and add another return statement that returns Internal Server Error

--- src/main/java/ma/emsi/nour/tp3sabri/HelloResource.java
+++ src/main/java/ma/emsi/nour/tp3sabri/HelloResource.java
@@ -4,13 +4,14 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
     @Path("personnes/{nom}")
-    public String hello(@PathParam("nom") String nom) {
-        return "Hello, " + nom;
+    public Response hello(@PathParam("nom") String nom) {
+        return Response.serverError().entity("Internal Server Error").build();
 }
 }
