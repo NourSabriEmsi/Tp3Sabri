54e75b5355f805030f9afb09c0d9a043477ee0e2
NourSabriEmsi <Nour.Sabri@emsi-edu.ma>
2025-11-13 15:39:34 +0100

    Added two methods:  returns just the parameter value, and  returns a JSON array containing the parameter.

--- src/main/java/ma/emsi/nour/tp3sabri/HelloResource.java
+++ src/main/java/ma/emsi/nour/tp3sabri/HelloResource.java
@@ -1,17 +1,24 @@
 package ma.emsi.nour.tp3sabri;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
     @Path("personnes/{nom}")
     public Response hello(@PathParam("nom") String nom) {
-        return Response.serverError().entity("Internal Server Error").build();
-}
-}
+        return Response.serverError().entity("Internal Server Error").build();}
+    @GET
+    @Path("test/{valeur}")
+    @Produces("text/plain")
+    public String testParam(@PathParam("valeur") String valeur) {
+        return valeur;}
+    @GET
+    @Path("testArray/{valeur}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String[] testArray(@PathParam("valeur") String val) {
+        return new String[]{ val };}}
